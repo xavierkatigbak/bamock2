1a37c0ec3001074e80dba4ea9484b1b06a22360e
xavierkatigbak <79707085+xavierkatigbak@users.noreply.github.com>
2022-11-15 23:30:01 +0800

    Initial Version

---
+++ bamock.js
@@ -0,0 +1,212 @@
+function check() {
+    var c=0
+    var q1=document.bamock.question1.value;
+    var q2=document.bamock.question2.value;
+    var q3=document.bamock.question3.value;
+    var q4=document.bamock.question4.value;
+    var q5=document.bamock.question5.value;
+    var q6=document.bamock.question6.value;
+    var q7=document.bamock.question7.value;
+    var q8=document.bamock.question8.value;
+    var q9=document.bamock.question9.value;
+    var q10=document.bamock.question10.value;
+    var q11=document.bamock.question11.value;
+    var q12=document.bamock.question12.value;
+    var q13=document.bamock.question13.value;
+    var q14=document.bamock.question14.value;
+    var q15=document.bamock.question15.value;
+    var q16=document.bamock.question16.value;
+    var q17=document.bamock.question17.value;
+    var q18=document.bamock.question18.value;
+    var q19=document.bamock.question19.value;
+    var q20=document.bamock.question20.value;
+    var q21=document.bamock.question21.value;
+    var q22=document.bamock.question22.value;
+    var q23=document.bamock.question23.value;
+    var q24=document.bamock.question24.value;
+    var q25=document.bamock.question25.value;
+    var q26=document.bamock.question26.value;
+    var q27=document.bamock.question27.value;
+    var q28=document.bamock.question28.value;
+    var q29=document.bamock.question29.value;
+    var q30=document.bamock.question30.value;
+    var q31=document.bamock.question31.value;
+    var q32=document.bamock.question32.value;
+    var q33=document.bamock.question33.value;
+    var q34=document.bamock.question34.value;
+    var q35=document.bamock.question35.value;
+    var q36=document.bamock.question36.value;
+    var q37=document.bamock.question37.value;
+    var q38=document.bamock.question38.value;
+    var q39=document.bamock.question39.value;
+    var q40=document.bamock.question40.value;
+    var q41=document.bamock.question41.value;
+    var q42=document.bamock.question42.value;
+    var q43=document.bamock.question43.value;
+    var q44=document.bamock.question44.value;
+    var q45=document.bamock.question45.value;
+    var q46=document.bamock.question46.value;
+    var q47=document.bamock.question47.value;
+    var q48=document.bamock.question48.value;
+    var q49=document.bamock.question49.value;
+    var q50=document.bamock.question50.value;
+    var q51=document.bamock.question51.value;
+    var q52=document.bamock.question52.value;
+    var q53=document.bamock.question53.value;
+    var q54=document.bamock.question54.value;
+    var q55=document.bamock.question55.value;
+    var q56=document.bamock.question56.value;
+    var q57=document.bamock.question57.value;
+    var q58=document.bamock.question58.value;
+    var q59=document.bamock.question59.value;
+    var q60=document.bamock.question60.value;
+    var q61=document.bamock.question61.value;
+    var q62=document.bamock.question62.value;
+    var q63=document.bamock.question63.value;
+    var q64=document.bamock.question64.value;
+    var q65=document.bamock.question65.value;
+    var q66=document.bamock.question66.value;
+    var q67=document.bamock.question67.value;
+    var q68=document.bamock.question68.value;
+    var q69=document.bamock.question69.value;
+    var q70=document.bamock.question70.value;
+    var q71=document.bamock.question71.value;
+    var q72=document.bamock.question72.value;
+    var q73=document.bamock.question73.value;
+    var q74=document.bamock.question74.value;
+    var q75=document.bamock.question75.value;
+    var q76=document.bamock.question76.value;
+    var q77=document.bamock.question77.value;
+    var q78=document.bamock.question78.value;
+    var q79=document.bamock.question79.value;
+    var q80=document.bamock.question80.value;
+    var q81=document.bamock.question81.value;
+    var q82=document.bamock.question82.value;
+    var q83=document.bamock.question83.value;
+    var q84=document.bamock.question84.value;
+    var q85=document.bamock.question85.value;
+    var q86=document.bamock.question86.value;
+    var q87=document.bamock.question87.value;
+    var q88=document.bamock.question88.value;
+    var q89=document.bamock.question89.value;
+    var q90=document.bamock.question90.value;
+    var q91=document.bamock.question91.value;
+    var q92=document.bamock.question92.value;
+    var q93=document.bamock.question93.value;
+    var q94=document.bamock.question94.value;
+    var q95=document.bamock.question95.value;
+    var q96=document.bamock.question96.value;
+    var q97=document.bamock.question97.value;
+    var q98=document.bamock.question98.value;
+    var q99=document.bamock.question99.value;
+    var q100=document.bamock.question100.value;
+    var result=document.getElementById('result');
+    var bamock=document.getElementById("bamock");
+    if (q1=="Answer1") {c++}
+    if (q2=="Answer3") {c++}
+    if (q3=="Answer3") {c++}
+    if (q4=="Answer2") {c++}
+    if (q5=="Answer3") {c++}
+    if (q6=="Answer2") {c++}
+    if (q7=="Answer2") {c++}
+    if (q8=="Answer4") {c++}
+    if (q9=="Answer2") {c++}
+    if (q10=="Answer2") {c++}
+    if (q11=="Answer4") {c++}
+    if (q12=="Answer2") {c++}
+    if (q13=="Answer1") {c++}
+    if (q14=="Answer2") {c++}
+    if (q15=="Answer3") {c++}
+    if (q16=="Answer1") {c++}
+    if (q17=="Answer2") {c++}
+    if (q18=="Answer4") {c++}
+    if (q19=="Answer3") {c++}
+    if (q20=="Answer3") {c++}
+    if (q21=="Answer4") {c++}
+    if (q22=="Answer4") {c++}
+    if (q23=="Answer1") {c++}
+    if (q24=="Answer1") {c++}
+    if (q25=="Answer4") {c++}
+    if (q26=="Answer3") {c++}
+    if (q27=="Answer1") {c++}
+    if (q28=="Answer3") {c++}
+    if (q29=="Answer3") {c++}
+    if (q30=="Answer4") {c++}
+    if (q31=="Answer4") {c++}
+    if (q32=="Answer1") {c++}
+    if (q33=="Answer3") {c++}
+    if (q34=="Answer2") {c++}
+    if (q35=="Answer1") {c++}
+    if (q36=="Answer1") {c++}
+    if (q37=="Answer2") {c++}
+    if (q38=="Answer3") {c++}
+    if (q39=="Answer4") {c++}
+    if (q40=="Answer3") {c++}
+    if (q41=="Answer4") {c++}
+    if (q42=="Answer4") {c++}
+    if (q43=="Answer3") {c++}
+    if (q44=="Answer4") {c++}
+    if (q45=="Answer1") {c++}
+    if (q46=="Answer1") {c++}
+    if (q47=="Answer4") {c++}
+    if (q48=="Answer1") {c++}
+    if (q49=="Answer2") {c++}
+    if (q50=="Answer3") {c++}
+    if (q51=="Answer1") {c++}
+    if (q52=="Answer1") {c++}
+    if (q53=="Answer2") {c++}
+    if (q54=="Answer3") {c++}
+    if (q55=="Answer1") {c++}
+    if (q56=="Answer2") {c++}
+    if (q57=="Answer1") {c++}
+    if (q58=="Answer1") {c++}
+    if (q59=="Answer4") {c++}
+    if (q60=="Answer4") {c++}
+    if (q61=="Answer2") {c++}
+    if (q62=="Answer1") {c++}
+    if (q63=="Answer1") {c++}
+    if (q64=="Answer2") {c++}
+    if (q65=="Answer1") {c++}
+    if (q66=="Answer4") {c++}
+    if (q67=="Answer3") {c++}
+    if (q68=="Answer4") {c++}
+    if (q69=="Answer2") {c++}
+    if (q70=="Answer1") {c++}
+    if (q71=="Answer1") {c++}
+    if (q72=="Answer1") {c++}
+    if (q73=="Answer4") {c++}
+    if (q74=="Answer1") {c++}
+    if (q75=="Answer2") {c++}
+    if (q76=="Answer4") {c++}
+    if (q77=="Answer1") {c++}
+    if (q78=="Answer2") {c++}
+    if (q79=="Answer2") {c++}
+    if (q80=="Answer2") {c++}
+    if (q81=="Answer3") {c++}
+    if (q82=="Answer2") {c++}
+    if (q83=="Answer3") {c++}
+    if (q84=="Answer3") {c++}
+    if (q85=="Answer4") {c++}
+    if (q86=="Answer4") {c++}
+    if (q87=="Answer2") {c++}
+    if (q88=="Answer3") {c++}
+    if (q89=="Answer4") {c++}
+    if (q90=="Answer3") {c++}
+    if (q91=="Answer4") {c++}
+    if (q92=="Answer2") {c++}
+    if (q93=="Answer2") {c++}
+    if (q94=="Answer4") {c++}
+    if (q95=="Answer4") {c++}
+    if (q96=="Answer2") {c++}
+    if (q97=="Answer4") {c++}
+    if (q98=="Answer3") {c++}
+    if (q99=="Answer1") {c++}
+    if (q100=="Answer2") {c++}
+    bamock.style.display="none";
+
+    if (c<100) {
+        result.textContent='FAILED. Your score is  ' + c + '/100'
+    } else {
+        result.textContent='PASSED. Your score is  ' + c + '/100'
+    }
+}
